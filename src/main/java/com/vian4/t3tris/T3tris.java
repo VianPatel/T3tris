13c38d465f7dd5519fae9fe26ed4c349e70ef5d8
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2022-05-14 16:58:50 -0700

    change offset

--- src/main/java/com/vian4/t3tris/T3tris.java
+++ src/main/java/com/vian4/t3tris/T3tris.java
@@ -59,13 +59,15 @@ public class T3tris extends SimpleApplication {
 
         ChaseCamera chaseCam = new ChaseCamera(cam, center, inputManager);
         chaseCam.setSmoothMotion(true);
-        chaseCam.setLookAtOffset(Vector3f.UNIT_Y.mult(board.yLen() / 2));
+        chaseCam.setLookAtOffset(Vector3f.UNIT_Y.mult(board.yLen() / 4));
 
         float minDistance = (float) Math.sqrt(board.xLen() * board.xLen() + board.yLen() * board.yLen() + board.zLen() * board.zLen());
         chaseCam.setMinDistance(minDistance * 1.1f);
         chaseCam.setDefaultDistance(minDistance * 1.5f);
         chaseCam.setMaxDistance(minDistance * 2.5f);
         chaseCam.setInvertVerticalAxis(true);
+        chaseCam.setRotationSpeed(2.5f);
+        chaseCam.setZoomSensitivity(4.0f);
                 
         inputManager.addMapping("RotateX",
         // new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
