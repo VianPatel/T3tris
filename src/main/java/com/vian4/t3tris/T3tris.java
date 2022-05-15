b5e395c95845099ce3b7e0a536722aee61da4443
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2022-05-14 18:19:11 -0700

    begin gui work

--- src/main/java/com/vian4/t3tris/T3tris.java
+++ src/main/java/com/vian4/t3tris/T3tris.java
@@ -13,12 +13,15 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.input.ChaseCamera;
 import com.jme3.input.KeyInput;
-import com.jme3.input.MouseInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
-import com.jme3.input.controls.MouseAxisTrigger;
 import com.jme3.light.AmbientLight;
 import com.jme3.light.SpotLight;
+import com.simsilica.lemur.Button;
+import com.simsilica.lemur.Command;
+import com.simsilica.lemur.Container;
+import com.simsilica.lemur.GuiGlobals;
+import com.simsilica.lemur.Label;
 import com.vian4.t3tris.gamepiece.Cube;
 import com.vian4.t3tris.gamepiece.GamePiece;
 import com.vian4.t3tris.gamepiece.JPiece;
@@ -32,10 +35,41 @@ public class T3tris extends SimpleApplication {
     public void simpleInitApp() {
         setPauseOnLostFocus(true);
         setDisplayStatView(false);
-        //Box box = new Box(0.5f, 0.5f, 0.5f);
+
         board = new GameBoard(assetManager, 20, 24, 10, 10);
+        rootNode.attachChild(board.getBoxNode());
+        board.getBoxNode().setShadowMode(ShadowMode.CastAndReceive);
+        
+        initGui();  
+        initPlane();
+        initCamera();
+        initKeybinds();
+        initLighting();
+    }
 
+    private void initGui() {
+        GuiGlobals.initialize(this);
 
+        // Create a simple container for our elements
+        Container myWindow = new Container();
+        guiNode.attachChild(myWindow);
+
+        // Put it somewhere that we will see it
+        // Note: Lemur GUI elements grow down from the upper left corner.
+        myWindow.setLocalTranslation(300, 300, 0);
+
+        // Add some elements
+        myWindow.addChild(new Label("Hello, World."));
+        Button clickMe = myWindow.addChild(new Button("Click Me"));
+        clickMe.addClickCommands(new Command<Button>() {
+            @Override
+            public void execute(Button source) {
+                System.out.println("Clicked");
+            }
+        });
+    }
+
+    private void initPlane() {
         Node planeNode = new Node();
         int planePadding = 2;
         Geometry plane = new Geometry("Plane", new Quad(2 * planePadding + board.xLen(), 2 * planePadding + board.zLen()));
@@ -43,17 +77,15 @@ public class T3tris extends SimpleApplication {
         plane.setMaterial(board.getColor(ColorRGBA.Gray));
         planeNode.attachChild(plane);
         planeNode.setLocalTranslation(-planePadding - 0.5f, 0.0f, -planePadding - 0.5f);
+
         rootNode.attachChild(planeNode);
         planeNode.setShadowMode(ShadowMode.Receive);
+    }
 
-        rootNode.attachChild(board.getBoxNode());
-        board.getBoxNode().setShadowMode(ShadowMode.CastAndReceive);
-        
+    private void initCamera() {
         flyCam.setEnabled(false);
 
         Node center = new Node();
-        flyCam.setEnabled(false);
-
         rootNode.attachChild(center);
         center.setLocalTranslation(board.xLen() / 2, 0, board.zLen() / 2);
 
@@ -61,20 +93,23 @@ public class T3tris extends SimpleApplication {
         chaseCam.setSmoothMotion(true);
         chaseCam.setLookAtOffset(Vector3f.UNIT_Y.mult(board.yLen() / 4));
 
-        float minDistance = (float) Math.sqrt(board.xLen() * board.xLen() + board.yLen() * board.yLen() + board.zLen() * board.zLen());
+        float minDistance = (float) Math
+                .sqrt(board.xLen() * board.xLen() + board.yLen() * board.yLen() + board.zLen() * board.zLen());
         chaseCam.setMinDistance(minDistance * 1.1f);
         chaseCam.setDefaultDistance(minDistance * 1.5f);
         chaseCam.setMaxDistance(minDistance * 2.5f);
         chaseCam.setInvertVerticalAxis(true);
         chaseCam.setRotationSpeed(2.5f);
         chaseCam.setZoomSensitivity(4.0f);
-                
+    }
+
+    private void initKeybinds() {
         inputManager.addMapping("RotateX",
-        // new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
-                new KeyTrigger(KeyInput.KEY_E), 
+                // new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
+                new KeyTrigger(KeyInput.KEY_E),
                 new KeyTrigger(KeyInput.KEY_SLASH));
         inputManager.addMapping("RotateZ",
-                new KeyTrigger(KeyInput.KEY_Q), 
+                new KeyTrigger(KeyInput.KEY_Q),
                 new KeyTrigger(KeyInput.KEY_PERIOD));
         inputManager.addMapping("Right",
                 new KeyTrigger(KeyInput.KEY_RIGHT),
@@ -91,8 +126,9 @@ public class T3tris extends SimpleApplication {
                 new KeyTrigger(KeyInput.KEY_UP),
                 new KeyTrigger(KeyInput.KEY_W));
         inputManager.addListener(actionListener, "RotateX", "RotateZ", "Right", "Left", "Down", "Front", "Back");
+    }
 
-
+    private void initLighting() {
         AmbientLight al = new AmbientLight();
         al.setColor(ColorRGBA.White.mult(0.4f));
         rootNode.addLight(al);
@@ -102,35 +138,21 @@ public class T3tris extends SimpleApplication {
         spot.setSpotInnerAngle(15f * FastMath.DEG_TO_RAD); // inner light cone (central beam)
         spot.setSpotOuterAngle(35f * FastMath.DEG_TO_RAD); // outer light cone (edge of the light)
         spot.setColor(ColorRGBA.White.mult(1.6f)); // light color
-        //spot.setPosition(new Vector3f(0, 30, 2)); // shine from camera loc
+        // spot.setPosition(new Vector3f(0, 30, 2)); // shine from camera loc
         spot.setPosition(new Vector3f(-50, 250, 0));
-        spot.setDirection((new Vector3f(0, 0, 0)).subtract(new Vector3f(0, 30, 2)).normalize()); // shine forward from camera loc
+        spot.setDirection((new Vector3f(0, 0, 0)).subtract(new Vector3f(0, 30, 2)).normalize()); // shine forward from
+                                                                                                 // camera loc
         rootNode.addLight(spot);
 
-
-        final int SHADOWMAP_SIZE=1024;
+        final int SHADOWMAP_SIZE = 1024;
         SpotLightShadowRenderer slsr = new SpotLightShadowRenderer(assetManager, SHADOWMAP_SIZE);
         slsr.setLight(spot);
         viewPort.addProcessor(slsr);
-
 
         FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
         SSAOFilter ssaoFilter = new SSAOFilter(12.94f, 43.92f, 0.33f, 0.61f);
         fpp.addFilter(ssaoFilter);
         viewPort.addProcessor(fpp);
-
-
-
-        inputManager.addMapping("MouseRotateX",
-                new MouseAxisTrigger(MouseInput.AXIS_X, true));
-        inputManager.addMapping("MouseRotateX_negative",
-                new MouseAxisTrigger(MouseInput.AXIS_X, false));
-            inputManager.addMapping("MouseRotateY",
-                new MouseAxisTrigger(MouseInput.AXIS_Y, true));
-        inputManager.addMapping("MouseRotateY_negative",
-                new MouseAxisTrigger(MouseInput.AXIS_Y, false));
-        
-        org.lwjgl.input.Mouse.setCursorPosition(Main.WIDTH / 2, Main.HEIGHT / 2);
     }
 
 
@@ -170,30 +192,6 @@ public class T3tris extends SimpleApplication {
             }
             //board.currentPiece().rotate();
         }
-        
-    }
-
-    public boolean pieceContainsPt(GamePiece piece, int x, int y, int z) {
-        if (piece == null)
-            return false;
-        for (Point point : piece.getPoints()) {
-            if (point.getX() == x && point.getY() == y && point.getZ() == z) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public GamePiece getRandomPiece() {
-        int rand = (int) (Math.random() * 4);
-        if (rand == 0) {
-            return new Cube(board, ColorRGBA.Blue, 2, 23, 0);
-        } else if (rand == 1) {
-            return new JPiece(board, ColorRGBA.Green, 2, 23, 0);
-        } else if (rand == 2) {
-            return new LPiece(board, ColorRGBA.Red, 2, 23, 0);
-        }
-        return new TPiece(board, ColorRGBA.Orange, 2, 23, 0);
     }
 
     private final ActionListener actionListener = new ActionListener() {
@@ -231,6 +229,19 @@ public class T3tris extends SimpleApplication {
             }
         }
     };
+
+    public GamePiece getRandomPiece() {
+        int rand = (int) (Math.random() * 4);
+        if (rand == 0) {
+            return new Cube(board, ColorRGBA.Blue, 2, 23, 0);
+        } else if (rand == 1) {
+            return new JPiece(board, ColorRGBA.Green, 2, 23, 0);
+        } else if (rand == 2) {
+            return new LPiece(board, ColorRGBA.Red, 2, 23, 0);
+        }
+        return new TPiece(board, ColorRGBA.Orange, 2, 23, 0);
+    }
+
 }
 
 /*
