87193845851aa47cd084a373f9831a960714c67a
Vian Patel <69381882+VianPatel@users.noreply.github.com>
2022-05-25 13:37:39 -0700

    added mouse tutorial

--- src/main/java/com/vian4/t3tris/gui/Tutorial.java
+++ src/main/java/com/vian4/t3tris/gui/Tutorial.java
@@ -36,18 +36,33 @@ public class Tutorial extends GuiState {
     private void updatePage() {
         selfNode.detachAllChildren();
         setupBasicFunction();
+        
+        int minDimension = Math.min(t3tris.getWidth(), t3tris.getHeight());
+        float scale;
+        float height;
+        float width;
         switch (page) {
             case 1:
-                Picture pic = new Picture("Keybinds");
-                pic.setImage(t3tris.getAssetManager(), "TutorialKeybinds.png", true);
-                int minDimension = Math.min(t3tris.getWidth(), t3tris.getHeight());
-                float scale = 0.9f;
-                float height = minDimension * (0.729166f) * scale;
-                float width = minDimension * scale;
-                pic.setWidth(width);
-                pic.setHeight(height);
-                pic.setPosition((t3tris.getWidth() - width) / 2, (t3tris.getHeight() - height) / 2);
-                selfNode.attachChild(pic);
+                Picture keybinds = new Picture("Keybinds");
+                keybinds.setImage(t3tris.getAssetManager(), "TutorialKeybinds.png", true);
+                scale = 0.9f;
+                height = minDimension * (0.729166f) * scale;
+                width = minDimension * scale;
+                keybinds.setWidth(width);
+                keybinds.setHeight(height);
+                keybinds.setPosition((t3tris.getWidth() - width) / 2, (t3tris.getHeight() - height) / 2);
+                selfNode.attachChild(keybinds);
+                break;
+            case 2:
+                Picture mouseControls = new Picture("MouseControls");
+                mouseControls.setImage(t3tris.getAssetManager(), "TutorialMouseControls.png", true);
+                scale = 0.9f;
+                height = minDimension * (0.729166f) * scale;
+                width = minDimension * scale;
+                mouseControls.setWidth(width);
+                mouseControls.setHeight(height);
+                mouseControls.setPosition((t3tris.getWidth() - width) / 2, (t3tris.getHeight() - height) / 2);
+                selfNode.attachChild(mouseControls);
                 break;
             default:
                 t3tris.getStateManager().detach(t3tris.getStateManager().getState(Tutorial.class));
